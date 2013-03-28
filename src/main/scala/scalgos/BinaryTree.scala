01c33dd8b4b9768b48f1749bc8e8635407aa342e
Pathikrit Bhowmick <pathikritbhowmick@msn.com>
2013-03-28 01:17:57 -0700

    Adding binary tree algos

---
+++ src/main/scala/scalgos/BinaryTree.scala
@@ -0,0 +1,52 @@
+package scalgos
+
+import Ordering.Implicits._
+
+object BinaryTree {
+
+  type Tree[T] = Option[Node[T]]
+
+  case class Node[T](left: Tree[T], entry: T, right: Tree[T])
+
+  /**
+   * Reconstruct a BST from its pre-order traversal in O(n * depth)
+   * Assume elements in BST are unique
+   *
+   * @param preOrder pre-order traversal of BST
+   * @return reconstructed BST
+   */
+  def reconstructBST[T](preOrder: Seq[T])(implicit ordering: Ordering[T]): Tree[T] = preOrder match {
+    case Nil => None
+    case (root :: children) =>
+      val (left, right) = children partition {_ < root}
+      Some(Node(reconstructBST(left), root, reconstructBST(right)))
+  }
+
+  /**
+   * Pre-order traverse a tree
+   *
+   * @param root the root of the tree
+   * @return the pre-order traversal
+   */
+  def preOrderTraversal[T](root: Tree[T]): Seq[T] = root match {
+    case None => Nil
+    case Some(Node(left, entry, right)) => Seq(entry) ++ preOrderTraversal(left) ++ preOrderTraversal(right)
+  }
+
+  /**
+   * Reconstruct a binary tree from its in-order and pre-order traversals in O(n * depth)
+   * Assume elements in tree are unique
+   * require (inOrder.length == preOrder.length)
+   *
+   * @param inOrder in-order traversal of binary tree
+   * @param preOrder pre-order traversal of binary tree
+   * @return reconstructed tree
+   */
+  def reconstruct[T](inOrder: Seq[T], preOrder: Seq[T]): Tree[T] = preOrder match {
+    case Nil => None
+    case root :: children =>
+      val (leftIn, head :: rightIn) = inOrder splitAt (inOrder indexOf root)
+      val (leftPre, rightPre) = children splitAt leftIn.length
+      Some(Node(reconstruct(leftIn, leftPre), root, reconstruct(rightIn, rightPre)))
+  }
+}
---
+++ src/test/scala/scalgos/BinaryTreeSpec.scala
@@ -0,0 +1,23 @@
+package scalgos
+
+import org.specs2.mutable.Specification
+
+import scalgos.BinaryTree._
+
+class BinaryTreeSpec extends Specification {
+
+  "reconstructBST" should {
+    "do a round-trip" in {
+      val preOrder = Seq(5, 3, 1, 2, 6)
+      preOrderTraversal(reconstructBST(preOrder)) must be equalTo(preOrder)
+    }
+  }
+
+  "reconstruct" should {
+    "do a round-trip" in {
+      val inOrder = Seq(1, 2, 3, 4, 5)
+      val preOrder = Seq(5, 4, 3, 2, 1)
+      preOrderTraversal(reconstruct(inOrder, preOrder)) must be equalTo(preOrder)
+    }
+  }
+}
