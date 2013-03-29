6408cccba057c1705c49d34c4bceab87c8a556df
Pathikrit Bhowmick <pathikritbhowmick@msn.com>
2013-03-29 15:17:57 -0700

    no warnings in build

--- build.sbt
+++ build.sbt
@@ -4,4 +4,7 @@ version := "0.0.1"
 
 scalaVersion := "2.10.1-RC1"
 
+scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature", "-language:postfixOps", "-language:implicitConversions")
+
 libraryDependencies += "org.specs2" %% "specs2" % "1.13" % "test"
+
--- src/main/scala/scalgos/games/Card.scala
+++ src/main/scala/scalgos/games/Card.scala
@@ -2,18 +2,25 @@ package scalgos.games
 
 import scala.collection.mutable
 
-case class Card(rank: Int, suit: Int) { override def toString = s"${Card ranks rank}${Card suits suit}" }
+case class Card(rank: Int, suit: Int) {
+  override def toString = s"${Card ranks rank}${Card suits suit}"
+}
 
 object Card {
   implicit val rankOrdering = Ordering by {card: Card => card.rank}
+
   val (ranks, suits) = ("23456789TJQKA", "♣♠♦♥")
+
   val all = for {rank <- 0 until ranks.length; suit <- 0 until suits.length} yield Card(rank, suit)
+
   implicit def fromStr(s: String) = Card((ranks indexOf s(0).toUpper), ("CSDH" indexOf s(1).toUpper))
 }
 
 class Deck {
   val cards = mutable.Queue() ++ util.Random.shuffle(Card.all)
+
   def deal = cards dequeue
+
   def remove(discards: Set[Card]) { discards foreach {card: Card => cards dequeueFirst (_ == card)} }
 }
 
@@ -29,15 +36,15 @@ object PokerHandType extends Enumeration {
     val isStraight = groups.size == 1 && (hand.max.rank - hand.min.rank) == 4 || isWheel
     val (isThreeOfAKind, isOnePair) = (groups contains 3, groups contains 2)
 
-    val handType = if (isStraight && isFlush)   StraightFlush
-    else if (groups contains 4)                 FourOfAKind
-    else if (isThreeOfAKind && isOnePair)       FullHouse
-    else if (isFlush)                           Flush
-    else if (isStraight)                        Straight
-    else if (isThreeOfAKind)                    ThreeOfAKind
-    else if (isOnePair && groups(2).size == 4)  TwoPair
-    else if (isOnePair)                         OnePair
-    else                                        HighCard
+    val handType = if (isStraight && isFlush)     StraightFlush
+      else if (groups contains 4)                 FourOfAKind
+      else if (isThreeOfAKind && isOnePair)       FullHouse
+      else if (isFlush)                           Flush
+      else if (isStraight)                        Straight
+      else if (isThreeOfAKind)                    ThreeOfAKind
+      else if (isOnePair && groups(2).size == 4)  TwoPair
+      else if (isOnePair)                         OnePair
+      else                                        HighCard
 
     val kickers = (1 until 5 flatMap groups.get).flatten.reverse
     require(hand.size == 5 && kickers.size == 5)
