3fd6ed1a1c86e889cd107342ac1ded80986d9eb4
Pathikrit Bhowmick <pathikritbhowmick@msn.com>
2013-04-04 19:34:55 -0700

    first cut at sudoku

--- src/main/scala/scalgos/games/Sudoku.scala
+++ src/main/scala/scalgos/games/Sudoku.scala
@@ -3,10 +3,40 @@ package scalgos.games
 
 object Sudoku {
 
-  def solve(sudoku: Array[Array[Int]], cell: Int = 0) {
+  type Board = Array[Array[Int]]
+
+  def solve(sudoku: Board, cell: Int = 0): Option[Board] = {
     if (cell == 9*9) {
-      return
+      return if (isValid(sudoku)) Some(sudoku) else None
+    }
+
+    val (r, c) = (cell%9, cell/9)
+
+    if(sudoku(r)(c) > 0) {
+      return solve(sudoku, cell+1)
+    }
+
+    def row(i: Int) = sudoku(r)(i)
+    def col(i: Int) = sudoku(i)(c)
+    def box(i: Int) = sudoku(3*(r/3) + i/3)(3*(c/3) + i%3)
+
+    def good(x: Int) = (0 until 9) forall {i => x != row(i) && x != col(i) && x != box(i)}
+
+    def guess(x: Int) = {
+      sudoku(r)(c) = x
+      solve(sudoku, cell+1)
+    }
+
+    val solutions = (1 to 9) filter good flatMap guess
+
+    if (solutions isEmpty) {
+      sudoku(r)(c) = 0
+      None
+    } else {
+      Some(solutions.head)
     }
   }
+
+  def isValid(sudoku: Board) = true
 
 }
