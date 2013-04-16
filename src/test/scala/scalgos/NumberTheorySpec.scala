d21d9d2cd89520229d66d7e2428ede549c1e0267
Pathikrit Bhowmick <pathikritbhowmick@msn.com>
2013-04-16 00:39:58 -0700

    unit test fibonacci

--- src/test/scala/scalgos/NumberTheorySpec.scala
+++ src/test/scala/scalgos/NumberTheorySpec.scala
@@ -7,9 +7,21 @@ import scalgos.NumberTheory._
 class NumberTheorySpec extends Specification {
 
   "catalan" should {
+    "fail for engative numbers" in todo
+
     "match known sequence" in {
       val expected: List[BigInt] = List(1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862, 16796)
       ((0 to 10) map catalan).toList must be equalTo expected
+    }
+  }
+
+  "fibonacci" should {
+    "fail for engative numbers" in todo
+
+    "match known sequence" in {
+      val expected: List[BigInt] = List(0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55)
+      ((0 to 10) map fibonacci).toList must be equalTo expected
+      fibonacci(99) must be equalTo BigInt("218922995834555169026")
     }
   }
 }
