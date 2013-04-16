5d50ed93a32b949db6b3211cf1d2cddf327fa4fa
Pathikrit Bhowmick <pathikritbhowmick@msn.com>
2013-04-16 00:26:22 -0700

    fix build

--- build.sbt
+++ build.sbt
@@ -6,5 +6,7 @@ scalaVersion := "2.10.1-RC1"
 
 scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature", "-language:postfixOps", "-language:implicitConversions")
 
-libraryDependencies += "org.specs2" %% "specs2" % "1.13" % "test"
+libraryDependencies ++= Seq(
+    "org.specs2" %% "specs2" % "1.13" % "test"
+)
 
--- src/test/scala/scalgos/MaxAreaInHistogramSpec.scala
+++ src/test/scala/scalgos/MaxAreaInHistogramSpec.scala
@@ -24,14 +24,14 @@ class MaxAreaInHistogramSpec extends Specification {
       apply(dims) must be equalTo 58
     }
 
-    "fail for negative inputs" in todo
+    "fail for negative or zero widths inputs" in todo
 
     "match the divide and conquer algorithm" in {
-      val blocks = RandomData.positiveSeq() zip RandomData.positiveSeq()
+      val blocks = RandomData.seq(min = 1) zip RandomData.seq(min = 1)
       val bars = for {b <- blocks; i <- 1 to b._2} yield b._1  //break block of width w into w blocks of width 1
       val expectedArea = DivideAndConquer.maxRectangleInHistogram(bars)
       apply(blocks) must be equalTo expectedArea
       apply(bars map {(_, 1)}) must be equalTo expectedArea
-    }.pendingUntilFixed("TODO: This has a bug - run it 100s of time till failure on small inputs")
+    }
   }
 }
