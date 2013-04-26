ec92487f31841d122f930c0fca1a1027bcbc1bc1
Pathikrit Bhowmick <pathikritbhowmick@msn.com>
2013-04-25 21:40:53 -0700

    checking in bithacks

---
+++ src/main/scala/com/github/pathikrit/scalgos/BitHacks.scala
@@ -0,0 +1,16 @@
+package com.github.pathikrit.scalgos
+
+import collection.BitSet
+
+/**
+ * Collection of bit hacks - Hacker's Delight style
+ */
+object BitHacks {
+
+  /**
+   * @return flip case of c
+   */
+  def swapCase(c: Char) = (c^32).toChar
+
+
+}
---
+++ src/test/scala/com/github/pathikrit/scalgos/BitHacksSpec.scala
@@ -0,0 +1,21 @@
+package com.github.pathikrit.scalgos
+
+import org.specs2.mutable.Specification
+
+import BitHacks._
+
+class BitHacksSpec extends Specification {
+
+  "swapCase" should {
+    "work for characters" in todo
+//    {
+//      val lower = "abcdefghijklmnopqrstuvwxyz"
+//      val upper = lower map swapCase
+//      upper must be equalTo lower.toUpperCase
+//      val lower2 = upper map swapCase
+//      lower2 must be equalTo lower
+//    }
+
+    "fail for non characters" in todo
+  }
+}
