391dfa4dd53ce9b8909a3deff69c31d8e3c48abc
Pathikrit Bhowmick <pathikritbhowmick@msn.com>
2013-04-26 01:06:10 -0700

    bithacks has some swap examples

--- src/main/scala/com/github/pathikrit/scalgos/BitHacks.scala
+++ src/main/scala/com/github/pathikrit/scalgos/BitHacks.scala
@@ -1,16 +1,34 @@
 package com.github.pathikrit.scalgos
 
-import collection.BitSet
-
 /**
  * Collection of bit hacks - Hacker's Delight style
  */
 object BitHacks {
 
   /**
-   * @return flip case of c
+   * @return toggle case of c
    */
-  def swapCase(c: Char) = (c^32).toChar
+  def toggleCase(c: Char) = (c^32).toChar
 
+  /**
+   * @return the tuple swapped
+   */
+  def xorSwap(a: (Int, Int)) = {
+    var (x,y) = a
+    x ^= y
+    y ^= x
+    x ^= y
+    (x,y)
+  }
 
+  /**
+   * @return the tuple swapped
+   */
+  def noTempSwap(a: (Int, Int)) = {
+    var (x,y) = a
+    x = x - y
+    y = x + y
+    x = y - x
+    (x,y)
+  }
 }
--- src/test/scala/com/github/pathikrit/scalgos/BitHacksSpec.scala
+++ src/test/scala/com/github/pathikrit/scalgos/BitHacksSpec.scala
@@ -2,20 +2,31 @@ package com.github.pathikrit.scalgos
 
 import org.specs2.mutable.Specification
 
+import Implicits.Crossable
 import BitHacks._
 
 class BitHacksSpec extends Specification {
 
-  "swapCase" should {
+  "flipCase" should {
     "work for characters" in todo
 //    {
 //      val lower = "abcdefghijklmnopqrstuvwxyz"
-//      val upper = lower map swapCase
+//      val upper = lower map flipCase
 //      upper must be equalTo lower.toUpperCase
-//      val lower2 = upper map swapCase
+//      val lower2 = upper map flipCase
 //      lower2 must be equalTo lower
 //    }
 
     "fail for non characters" in todo
   }
+
+  "swap" should {
+    "work" in {
+      for (v <- (-100 to 100) X (-100 to 100)) {
+        xorSwap(v) must be equalTo v.swap
+        noTempSwap(v) must be equalTo v.swap
+      }
+    }
+  }
+
 }
