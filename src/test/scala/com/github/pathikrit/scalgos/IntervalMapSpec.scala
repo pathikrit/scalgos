d57c95cb1d20d956c208bc8398ea5874a4a8ce22
pathikrit <pathikritbhowmick@msn.com>
2014-04-16 22:12:14 -0700

    randomized testing for interval map

--- src/test/scala/com/github/pathikrit/scalgos/IntervalMapSpec.scala
+++ src/test/scala/com/github/pathikrit/scalgos/IntervalMapSpec.scala
@@ -20,57 +20,31 @@ class IntervalMapSpec extends Specification {
 
       def clear(r: Interval) = r foreach segments.remove
 
-      def toSeq = {
-        //val valMap = segments groupBy {_._2} mapValues {_.keySet} mapValues meld
-        Nil
-      }
+      def toSeq = throw new UnsupportedOperationException
 
-      def meld(s: Set[Int]) = {
-        var intervals = Seq.empty[Interval]
-
-        val max = s.max
-        for (start <- s.min to max if intervals.isEmpty || start >= intervals.last.end) {
-          var end = 0
-          for(i <- start to max if s contains i) {
-            end = i
-          }
-          intervals = intervals :+ Interval(start, end)
-        }
-        intervals
-      }
+      override def toString = segments.toSeq sortBy {_._1} map {case (i, v) => s"$i: $v"} mkString ", "
     }
-    import RandomData._
 
     val map1 = IntervalMap.empty[Int]
     val map2 = new DumbIntervalMap[Int]
-    map1 must be equalTo map2
+    val (n, r, p, v) = (10000, 100, 0.8, 20)
 
     examplesBlock {
-      for (i <- 1 to 1000) {
-        val start = integer()
-        val end = integer(start = start)
-        if(number() < 0.8) {
-          val value = integer(20)
+      import RandomData._
+      for (i <- 1 to n) {
+        val start = integer(-n, n)
+        val end = integer(start, n+1)
+        if(number() < p) {
+          val value = integer(v)
           map1(start -> end) = value
           map2(start -> end) = value
         } else {
           map1 clear (start -> end)
           map2 clear (start -> end)
         }
-        map1 must be equalTo map2
+
+        (-2*r to 2*r) forall {i => map1(i) == map2(i)} must beTrue
       }
     }
-
-    todo
-  }
-
-  "work for small case" in {
-    val map = IntervalMap.empty[String]
-    map(10 -> 90) = "a"
-    map(20 -> 30) = "b"
-    map(40 -> 50) = "c"
-    map(25 -> 45) = "d"
-    map(50 -> 100) = "c"
-    map.toString mustEqual "{[10, 20) : a, [20, 25) : b, [25, 45) : d, [45, 100) : c}"
   }
 }
