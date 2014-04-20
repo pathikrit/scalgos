004e9d973110ea3ed45d7a60049a80c2328d8599
pathikrit <pathikritbhowmick@msn.com>
2014-04-20 13:50:33 -0700

    move to memo to own code file

--- src/main/scala/com/github/pathikrit/scalgos/DynamicProgramming.scala
+++ src/main/scala/com/github/pathikrit/scalgos/DynamicProgramming.scala
@@ -6,19 +6,6 @@ import collection.mutable
 import Implicits._
 
 /**
- * Generic way to create memoized functions (even recursive and multiple-arg ones)
- *
- * @param f the function to memoize
- * @tparam A input
- * @tparam B output
- */
-case class Memo[A, B](f: A => B) extends (A => B) {
-  import collection.mutable.{Map => Dict}
-  private val cache = Dict.empty[A, B]
-  override def apply(x: A) = cache getOrElseUpdate (x, f(x))
-}
-
-/**
  * Collection of DP algorithms
  */
 object DynamicProgramming {
