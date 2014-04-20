ad6cbb1cc9e81ff8e507620892be2c449ff5822b
pathikrit <pathikritbhowmick@msn.com>
2014-04-20 16:52:20 -0700

    cleanup Memo.scala

--- src/main/scala/com/github/pathikrit/scalgos/Memo.scala
+++ src/main/scala/com/github/pathikrit/scalgos/Memo.scala
@@ -1,24 +1,22 @@
 package com.github.pathikrit.scalgos
 
-import collection.mutable.{Map => Dict}
-
 /**
  * Generic way to create memoized functions (even recursive and multiple-arg ones)
  *
  * @param f the function to memoize
- * @tparam A1 input to f
- * @tparam A2 the keys we should use in cache instead of A1
- * @tparam B output of f
+ * @tparam I1 input to f
+ * @tparam I2 the keys we should use in cache instead of I1
+ * @tparam O output of f
  */
-case class Memo[A1 <% A2, A2, B](f: A1 => B) extends (A1 => B) {
-  val cache = Dict.empty[A2, B]
-  override def apply(x: A1) = cache getOrElseUpdate (x, f(x))
+case class Memo[I1 <% I2, I2, O](f: I1 => O) extends (I1 => O) {
+  import collection.mutable.{Map => Dict}
+  val cache = Dict.empty[I2, O]
+  override def apply(x: I1) = cache getOrElseUpdate (x, f(x))
 }
 
 object Memo {
-
   /**
-   * Type of a simple memoized function
+   * Type of a simple memoized function e.g. when A1 = A2
    */
-  type F[A, B] = Memo[A, A, B]
+  type F[I, O] = Memo[I, I, O]
 }
