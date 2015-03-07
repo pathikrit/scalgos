0305f7a65ba4a998cf5821a3520d369d94662cad
Pathikrit Bhowmick <pathikritbhowmick@msn.com>
2015-03-07 02:15:46 -0800

    shorten sudoku solver code

--- src/main/scala/com/github/pathikrit/scalgos/games/Sudoku.scala
+++ src/main/scala/com/github/pathikrit/scalgos/games/Sudoku.scala
@@ -9,35 +9,25 @@ object Sudoku {
 
   /**
    * Sudoku solver
-   * O(average elementary branching factor ^ empty cells
+   * O(average elementary branching factor ^ empty cells)
    *
-   * @param sudoku input game to be solved
+   * @param board input game to be solved
    * @param cell current cell (0 at start, 81 at end)
    * @return true iff solution exists (the sudoku board is modified in place with solution else left alone)
    */
-  def solve(sudoku: Board, cell: Int = 0): Boolean = {
-    if (cell == 9*9) {
-      return isSolution(sudoku)
-    }
+  def solve(board: Board, cell: Int = 0): Boolean = (cell%9, cell/9) match {
+    case (_, 9) => isSolution(board)
+    case (r, c) if board(r)(c) > 0 => solve(board, cell+1)
+    case (r, c) =>
+      def cells(i: Int) = board(r)(i) :: board(r)(i) :: board(3*(r/3) + i/3)(3*(c/3) + i%3) :: Nil
+      val used = 0 until 9 flatMap cells
 
-    val (r, c) = (cell%9, cell/9)
+      def guess(x: Int) = {
+        board(r)(c) = x
+        solve(board, cell+1)
+      }
 
-    if(sudoku(r)(c) > 0) {
-      return solve(sudoku, cell+1)
-    }
-
-    def row(i: Int) = sudoku(r)(i)
-    def col(i: Int) = sudoku(i)(c)
-    def box(i: Int) = sudoku(3*(r/3) + i/3)(3*(c/3) + i%3)
-
-    def good(x: Int) = (0 until 9) forall {i => x != row(i) && x != col(i) && x != box(i)}
-
-    def guess(x: Int) = {
-      sudoku(r)(c) = x
-      solve(sudoku, cell+1)
-    }
-
-    ((1 to 9) filter good exists guess) || {sudoku(r)(c) = 0; false}
+      (1 to 9 diff used exists guess) || {board(r)(c) = 0; false}
   }
 
   /**
