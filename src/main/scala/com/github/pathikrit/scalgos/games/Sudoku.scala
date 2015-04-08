938d832f26ba12b5b6f3dc4b1fe35509da2a24df
Pathikrit Bhowmick <pathikritbhowmick@msn.com>
2015-04-08 01:20:38 -0700

    sudoku solver refactor + tests

--- src/main/scala/com/github/pathikrit/scalgos/games/Sudoku.scala
+++ src/main/scala/com/github/pathikrit/scalgos/games/Sudoku.scala
@@ -2,53 +2,47 @@ package com.github.pathikrit.scalgos.games
 
 /**
  * Sudoku related algorithms
+ * @param board underlying board (0 for empty cells)
  */
-object Sudoku {
-
-  type Board = Array[Array[Int]]
+case class Sudoku(board: IndexedSeq[IndexedSeq[Int]]) {
 
   /**
    * Sudoku solver
    * O(average elementary branching factor ^ empty cells)
    *
-   * @param board input game to be solved
    * @param cell current cell (0 at start, 81 at end)
-   * @return true iff solution exists (the sudoku board is modified in place with solution else left alone)
+   * @return Some(solved board) if solution exists; else None
    */
-  def solve(board: Board, cell: Int = 0): Boolean = (cell%9, cell/9) match {
-    case (_, 9) => isSolution(board)
-    case (r, c) if board(r)(c) > 0 => solve(board, cell+1)
+  def solve(cell: Int = 0): Option[Sudoku] = (cell%9, cell/9) match {
+    case (r, 9) => Some(this)
+    case (r, c) if board(r)(c) > 0 => solve(cell + 1)
     case (r, c) =>
-      def cells(i: Int) = board(r)(i) :: board(r)(i) :: board(3*(r/3) + i/3)(3*(c/3) + i%3) :: Nil
+      def cells(i: Int) = board(r)(i) :: board(i)(c) :: board(3*(r/3) + i/3)(3*(c/3) + i%3) :: Nil
       val used = 0 until 9 flatMap cells
 
-      def guess(x: Int) = {
-        board(r)(c) = x
-        solve(board, cell+1)
-      }
-
-      (1 to 9 diff used exists guess) || {board(r)(c) = 0; false}
+      def guess(x: Int) = Sudoku(board.updated(r, board(r).updated(c, x))).solve(cell+1)
+      1 to 9 diff used collectFirst Function.unlift(guess)
   }
 
   /**
-   * @return true iff sudoku is in valid solved state
+   * @return true iff board is in valid solved state
    */
-  def isSolution(sudoku: Board) = true
+  def isSolution = true
 
   /**
    * @return pretty print sudoku board
    */
-  def pretty(sudoku: Board) = {
+  override def toString = {
     var pretty = ""
-    for (i <- sudoku.indices) {
+    for (i <- board.indices) {
       if(i == 3 || i == 6) {
         pretty += "- - - - - - - - - - -\n"
       }
-      for (j <- sudoku(i).indices) {
+      for (j <- board(i).indices) {
         if (j == 3 || j == 6) {
           pretty += "| "
         }
-        pretty += sudoku(i)(j) + " "
+        pretty += board(i)(j) + " "
       }
       pretty += "\n"
     }
--- src/test/scala/com/github/pathikrit/scalgos/games/SudokuSpec.scala
+++ src/test/scala/com/github/pathikrit/scalgos/games/SudokuSpec.scala
@@ -2,8 +2,6 @@ package com.github.pathikrit.scalgos.games
 
 import org.specs2.mutable.Specification
 
-import Sudoku._
-
 class SudokuSpec extends Specification {
 
   "solve" should {
@@ -11,11 +9,38 @@ class SudokuSpec extends Specification {
     "fail on boards that contain anything other than 0-9" in todo
     "false on invalid solved boards" in todo
     "false and input not modified on insolvable boards" in todo
-    "solve a normal solution" in todo
-    "find multiple solutions" in todo
 
-    "work on the fully empty board" in {
-      solve(Array.fill(9, 9)(0)) must beTrue
+    "solve a normal solution" in {
+      import scala.collection.{IndexedSeq => $}
+
+      val board = $(
+        $(1, 0, 0, 0, 0, 7, 0, 9, 0),
+        $(0, 3, 0, 0, 2, 0, 0, 0, 8),
+        $(0, 0, 9, 6, 0, 0, 5, 0, 0),
+        $(0, 0, 5, 3, 0, 0, 9, 0, 0),
+        $(0, 1, 0, 0, 8, 0, 0, 0, 2),
+        $(6, 0, 0, 0, 0, 4, 0, 0, 0),
+        $(3, 0, 0, 0, 0, 0, 0, 1, 0),
+        $(0, 4, 0, 0, 0, 0, 0, 0, 7),
+        $(0, 0, 7, 0, 0, 0, 3, 0, 0)
+      )
+
+      val solution = $(
+        $(1, 6, 2, 8, 5, 7, 4, 9, 3),
+        $(5, 3, 4, 1, 2, 9, 6, 7, 8),
+        $(7, 8, 9, 6, 4, 3, 5, 2, 1),
+        $(4, 7, 5, 3, 1, 2, 9, 8, 6),
+        $(9, 1, 3, 5, 8, 6, 7, 4, 2),
+        $(6, 2, 8, 7, 9, 4, 1, 3, 5),
+        $(3, 5, 6, 4, 7, 8, 2, 1, 9),
+        $(2, 4, 1, 9, 3, 5, 8, 6, 7),
+        $(8, 9, 7, 2, 6, 1, 3, 5, 4)
+      )
+
+      Sudoku(board).solve() must beSome(Sudoku(solution))
     }
+
+    "find multiple solutions" in todo
+    "work on the fully empty board" in todo
   }
 }
