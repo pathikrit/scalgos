c0f2f7f47d47fff3071f44cb5a951bebab6ed0aa
Pathikrit Bhowmick <pathikritbhowmick@msn.com>
2015-04-26 19:59:34 -0500

    shorter greedy max rectangle solution

--- src/main/scala/com/github/pathikrit/scalgos/Greedy.scala
+++ src/main/scala/com/github/pathikrit/scalgos/Greedy.scala
@@ -1,7 +1,5 @@
 package com.github.pathikrit.scalgos
 
-import scala.annotation.tailrec
-
 /**
  * Collection of greedy algorithms
  */
@@ -10,22 +8,22 @@ object Greedy {
   /**
    * Stack based solution to maximum rectangle in histogram problem
    * stack always has (h, x) such that h is increasing order x is the earliest index at which h can be spanned
-   * O(n)
+   * O(n) - TODO: Proof?
+   * TODO: Move the longer rule based solution to gist and move its test into greedy/divide and conquer
    *
    * @param heights heights of histogram
    * @return area of largest rectangle under histogram
    */
   def maxRectangleInHistogram(heights: List[Int]): Int = {
-    @tailrec
-    def solve(stack: List[(Int, Int)], best: Int, remaining: List[(Int, Int)]): Int = (stack, remaining) match {
-      case (Nil, Nil) => best
-      case ((y, x) :: ss, Nil) => solve(ss, best max (y*(heights.length - x)), remaining)
+    def solve(stack: List[(Int, Int)], remaining: List[(Int, Int)]): Int = (stack, remaining) match {
+      case (Nil, Nil) => 0
+      case ((y, x) :: rest, Nil) => solve(rest, remaining) max (y*(heights.length - x))
       case (_, (h, i) :: hs) =>
         val (taller, shorter) = stack.span(_._1 > h)
-        val newBest = taller.foldLeft(best){case (a, (y, x)) => a max (y * (i - x))}
         val pos = taller.lastOption.map(_._2) getOrElse i
-        solve((h, pos) :: shorter, newBest, hs)
+        val best = solve((h, pos) :: shorter, hs)
+        taller.foldLeft(best){case (a, (y, x)) => a max (y * (i - x))}
     }
-    solve(Nil, 0, heights.zipWithIndex)
+    solve(Nil, heights.zipWithIndex)
   }
 }
