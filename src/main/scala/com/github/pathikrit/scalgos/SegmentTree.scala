418f87593521f7fd06026373272370d430cd6461
Pathikrit Bhowmick <pathikritbhowmick@msn.com>
2016-06-04 01:31:19 -0700

    Make data structures use standard collection interfaces

--- src/main/scala/com/github/pathikrit/scalgos/SegmentTree.scala
+++ src/main/scala/com/github/pathikrit/scalgos/SegmentTree.scala
@@ -2,10 +2,12 @@ package com.github.pathikrit.scalgos
 
 import scala.reflect.ClassTag
 
+import Implicits._
+
 /**
-  * Segment tree: A data structure that lets you quickly do fold on an interval
+  * Segment tree: A data structure that lets you update indices and fold over intervals
   */
-trait SegmentTree[A] extends (Int => A) {
+sealed trait SegmentTree[A] extends (Int => A) {
   def update(idx: Int, value: A): Unit
   def fold(z: A)(from: Int, to: Int): A
 }
@@ -17,16 +19,17 @@ object SegmentTree {
     * O(1) for apply
     * O(log n) for update and fold
     *
+    * @param f reducer commutative function, implementation can be changed to support non-commutative ones too
     * @tparam A
     */
   def apply[A: ClassTag](data: Traversable[A])(f: (A, A) => A) = new SegmentTree[A] {
     private[this] val n = data.size
-    private[this] val t = Array.ofDim[A](2 * n)
+    private[this] val t = Array.ofDim[A](2*n)
 
     private[this] def build(i: Int) = t(i) = f(t(2*i), t(2*i + 1))
 
     data.copyToArray(t, n)
-    (n - 1 until 0 by -1) foreach build
+    (n - 1) --> 1 foreach build
 
     override def apply(idx: Int) = t(idx + n)
 
@@ -41,10 +44,11 @@ object SegmentTree {
 
     override def fold(z: A)(from: Int, to: Int) = {
       var result = z
-      var (l, r) = (from + n, to + n)
+      var l = from + n
+      var r = to + n
       while (l <= r) {
         if (l%2 == 1) result = f(result, t(l))
-        if (r%2 == 0) result = f(result, t(r))
+        if (r%2 == 0) result = f(t(r), result)
         l = (l + 1)/2
         r = (r - 1)/2
       }
@@ -54,8 +58,8 @@ object SegmentTree {
 
   def naive[A: ClassTag](data: Traversable[A])(f: (A, A) => A) = new SegmentTree[A] {
     private[this] val array: Array[A] = data.toArray
-    override def apply(i: Int) = array(i)
+    override def apply(idx: Int) = array(idx)
     override def update(idx: Int, value: A) = array(idx) = value
-    override def fold(z: A)(from: Int, to: Int) = array.slice(from, to+1).foldLeft(z)(f)
+    override def fold(z: A)(from: Int, to: Int) = array.slice(from, to + 1).foldLeft(z)(f)
   }
 }
--- src/main/scala/com/github/pathikrit/scalgos/UnionFind.scala
+++ src/main/scala/com/github/pathikrit/scalgos/UnionFind.scala
@@ -1,14 +1,12 @@
 package com.github.pathikrit.scalgos
 
-import scala.collection.mutable
-
-import Implicits.when
+import scala.collection.{mutable, generic}
 
 /**
  * Simpler implementation of @see DisjointSet
  * TODO: test this with above
  */
-class UnionFind[A] {
+class UnionFind[A] extends PartialFunction[A, A] with generic.Growable[A] {
   private[this] val parent = mutable.Map.empty[A, A]
 
   private[this] def find(x: A): A = parent(x) match {
@@ -18,11 +16,16 @@ class UnionFind[A] {
       parent(x)
   }
 
-  def apply(x: A) = when(contains(x))(find(x))
+  override def isDefinedAt(x: A) = parent contains x
+
+  override def apply(x: A) = find(x)
+
+  override def +=(x: A) = {
+    parent(x) = x
+    this
+  }
+
+  override def clear() = parent.clear()
 
   def union(x: A, y: A) = parent(find(x)) = find(y)
-
-  def +=(x: A) = parent(x) = x
-
-  def contains(x: A) = parent contains x
 }
