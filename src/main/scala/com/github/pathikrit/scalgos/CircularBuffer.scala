478c353cbb7551c03be5a0eeb7f77b1f88208058
Pathikrit Bhowmick <pathikritbhowmick@msn.com>
2017-01-30 02:32:25 -0500

    Add circular buffer

---
+++ src/main/scala/com/github/pathikrit/scalgos/CircularBuffer.scala
@@ -0,0 +1,100 @@
+package com.github.pathikrit.scalgos
+
+import scala.collection.mutable
+import scala.reflect.ClassTag
+
+/**
+  * A data structure that provides O(1) get, update, length, append, prepend, clear, trimStart and trimRight
+  * @tparam A
+  */
+class CircularBuffer[A: ClassTag](initialSize: Int = 1<<4) extends mutable.Buffer[A] {
+  private var array = Array.ofDim[A](initialSize)
+  private var start, end = 0
+
+  override def apply(idx: Int) = assumingValidIndex(idx) {
+    array(mod(start + idx))
+  }
+
+  override def update(idx: Int, elem: A) = assumingValidIndex(idx) {
+    array(mod(start + idx)) = elem
+  }
+
+  override def length = mod(mod(end) - mod(start))
+
+  override def +=(elem: A) = {
+    if (size == array.length) resize()
+    array(mod(end)) = elem
+    end += 1
+    this
+  }
+
+  override def clear() = {
+    start = 0
+    end = 0
+  }
+
+  override def +=:(elem: A) = {
+    if (size == array.length) resize()
+    start -= 1
+    array(mod(start)) = elem
+    this
+  }
+
+  override def prependAll(xs: TraversableOnce[A]) =
+    xs.toSeq.reverse.foreach(x => x +=: this)
+
+  override def insertAll(idx: Int, elems: Traversable[A]) = {
+    if (idx == 0) {
+      prependAll(elems)
+    } else {
+      val shift = (idx until size).map(this)
+      end = start + idx
+      elems.foreach(+=)
+      shift.foreach(+=)
+    }
+  }
+
+  override def remove(idx: Int) = assumingValidIndex(idx) {
+    val ret = this(idx)
+    remove(idx, 1)
+    ret
+  }
+
+  override def remove(idx: Int, count: Int) = assumingValidIndex(idx) {
+    if (idx + count >= size) {
+      end = start + idx
+    } else if (count > 0) {
+      if (idx == 0) {
+        start += count
+      } else {
+        (0 until count).foreach(i => this(idx + i) = this(idx + count + i))
+        end -= count
+      }
+    }
+  }
+
+  override def iterator = indices.iterator.map(apply)
+
+  override def trimStart(n: Int) = if (n >= size) clear() else if (n >= 0) start += n
+
+  override def trimEnd(n: Int) = if (n >= size) clear() else if (n >= 0) end -= n
+
+  override def head = this(0)
+
+  override def last = this(size - 1)
+
+  private def mod(x: Int) = Math.floorMod(x, array.length)
+
+  private def resize(len: Int = 2 * array.length): Unit = {
+    val array2 = Array.ofDim[A](len)
+    copyToArray(array2) //TODO: optimize this by doing array.copy(array2, start, len) etc.
+    end = size
+    start = 0
+    array = array2
+  }
+
+  private def assumingValidIndex[U](idx: Int)(f: => U): U = {
+    if(!isDefinedAt(idx)) throw new IndexOutOfBoundsException(idx.toString)
+    f
+  }
+}
---
+++ src/test/scala/com/github/pathikrit/scalgos/CircularBufferSpec.scala
@@ -0,0 +1,28 @@
+package com.github.pathikrit.scalgos
+
+import org.specs2.mutable.Specification
+
+import scala.collection.mutable
+
+class CircularBufferSpec extends Specification {
+  "circular-buffer" should {
+    "match the library" in {
+      val buffer = new CircularBuffer[Int]()
+      val buffer2 = mutable.ArrayBuffer.empty[Int]
+
+      def apply[U](f: mutable.Buffer[Int] => U) = {
+        //val clue = s"Before: [buffer1=${buffer}; buffer2=${buffer2}]"
+        f(buffer)
+        f(buffer2)
+        (buffer shouldEqual buffer2)
+      }
+
+      apply(_.append(1, 2, 3, 4, 5))
+      apply(_.prepend(6, 7, 8))
+      apply(_.trimStart(2))
+      apply(_.trimEnd(3))
+      apply(_.insertAll(0, Seq(9, 10, 11)))
+      apply(_.insertAll(1, Seq(12, 13)))
+    }
+  }
+}
